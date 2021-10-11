c13611d7e5d3b37ab963bf933e4cfb67573b5642
Tristan Kernan <tristanmkernan@gmail.com>
2021-10-11 15:41:20 -0400

    fix: user admin list view

--- myauth/admin.py
+++ myauth/admin.py
@@ -3,4 +3,9 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-#admin.site.register(User, UserAdmin)
+class MyUserAdmin(UserAdmin):
+    list_display = ("username", "is_staff", "is_superuser")
+
+
+
+admin.site.register(User, MyUserAdmin)
